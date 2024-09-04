6c4389c28cf4f5127c5d3055e281c69b626fa4c6
Bell, Ryan <ryan.bell62101@gmail.com>
2024-09-03 20:39:24 -0500

    added eval_arg tests and wrote man page

--- src/util.rs
+++ src/util.rs
@@ -1,16 +1,21 @@
-use std::{error::Error, io::{stdin, stdout, Write}};
+use std::{env, error::Error};
 
 use crate::rcon::client::ClientConfig;
 
 // evaluate command line arguments, loading them into a ServerConfig object
 pub fn eval_args(args: &[String]) -> Result<ClientConfig, Box<dyn Error>> {
-    if args.len() < 2 {
-        return Err("invalid number of arguments".into());
-    }
-
-    let mut hostname = None;
-    let mut port = None;
-    let mut password = None;
+    let mut hostname = match env::var("RCON_HOSTNAME") {
+        Err(_) => None,
+        Ok(env_var) => Some(env_var)
+    };
+    let mut port = match env::var("RCON_PORT") {
+        Err(_) => None,
+        Ok(env_var) => Some(env_var.parse::<u16>()?)
+    };
+    let mut password = match env::var("RCON_PASSWORD") {
+        Err(_) => None,
+        Ok(env_var) => Some(env_var)
+    };
 
     let mut curr = 1;
     while curr < args.len() {
@@ -34,7 +39,7 @@ pub fn eval_args(args: &[String]) -> Result<ClientConfig, Box<dyn Error>> {
                 }
             }
             _ => {
-                return Err(format!("invalid arg at index {}", curr).into());
+                return Err(format!("invalid arg `{}`", args[curr]).into());
             }
         }
         curr += 1;
@@ -47,13 +52,7 @@ pub fn eval_args(args: &[String]) -> Result<ClientConfig, Box<dyn Error>> {
         return Err("port not provided (use `-p <port>` or `--port <port>`".into());
     }
     if password.is_none() {
-        let mut user_input = String::new();
-
-        print!("RCON password: ");
-        stdout().flush().unwrap();
-        stdin().read_line(&mut user_input).unwrap();
-
-        password = Some(user_input[..user_input.len()-1].to_string());
+        return Err("password not provided (use `-P <password>` or `--password <password>`".into());
     }
 
     Ok(ClientConfig{
@@ -65,10 +64,147 @@ pub fn eval_args(args: &[String]) -> Result<ClientConfig, Box<dyn Error>> {
 
 #[cfg(test)]
 mod tests {
-    //use super::*;
+    use std::env;
+
+    use super::*;
+  
+    // sets environment variables and returns old environment variables
+    fn set_env_vars(
+        hostname: Option<String>,
+        port: Option<String>,
+        password: Option<String>
+    ) -> (Option<String>, Option<String>, Option<String>) {
+        let old_hostname = match env::var("RCON_HOSTNAME") {
+            Err(_) => None,
+            Ok(env_var) => Some(env_var)
+        };
+        let old_port = match env::var("RCON_PORT") {
+            Err(_) => None,
+            Ok(env_var) => Some(env_var)
+        };
+        let old_password = match env::var("RCON_PASSWORD") {
+            Err(_) => None,
+            Ok(env_var) => Some(env_var)
+        };
+        
+        if let Some(hostname) = hostname {
+            env::set_var("RCON_HOSTNAME", hostname);
+        }
+        if let Some(port) = port {
+            env::set_var("RCON_PORT", port);
+        }
+        if let Some(password) = password {
+            env::set_var("RCON_PASSWORD", password);
+        }
+
+        (old_hostname, old_port, old_password)
+    }
 
     #[test]
     fn test_eval_args() {
-        
+        let args: Vec<String> = vec!["rust-rcon", "-P", "password", "-h", "minecraft.lan", "-p", "25575"]
+            .iter()
+            .map(|x| x.to_string())
+            .collect();
+        if let Ok(config) = eval_args(&args) {
+            assert_eq!(config.hostname, "minecraft.lan".to_string());
+            assert_eq!(config.port, 25575);
+            assert_eq!(config.password, "password");
+        } else {
+            assert!(false);
+        }
+        let args: Vec<String> = vec![
+            "rust-rcon",
+            "--password", "password",
+            "--hostname", "minecraft.lan",
+            "--port", "25575"
+        ]
+            .iter()
+            .map(|x| x.to_string())
+            .collect();
+        if let Ok(config) = eval_args(&args) {
+            assert_eq!(config.hostname, "minecraft.lan".to_string());
+            assert_eq!(config.port, 25575);
+            assert_eq!(config.password, "password");
+        } else {
+            assert!(false);
+        }
+
+        let (old_hostname, old_port, old_password) = set_env_vars(
+            Some("minecraft.lan".to_string()),
+            Some("25575".to_string()),
+            Some("password".to_string())
+        );
+
+        match eval_args(&vec!["rust-rcon".to_string()]) {
+            Ok(config) => {
+                assert_eq!(config.hostname, "minecraft.lan".to_string());
+                assert_eq!(config.port, 25575);
+                assert_eq!(config.password, "password");
+            },
+            Err(why) => {
+                println!("{}", why);
+                assert!(false);
+            }
+        }
+
+        set_env_vars(old_hostname, old_port, old_password);
+    }
+
+    #[test]
+    fn test_invalid_arguments() {
+        // invalid flag
+        let args = vec!["rust-rcon".to_string(), "-r".to_string()];
+
+        if let Err(why) = eval_args(&args) {
+            assert_eq!(why.to_string(), "invalid arg `-r`");
+        } else {
+            println!("expected argument error");
+            assert!(false);
+        }
+
+        // invalid port (not a number)
+        let args = vec![
+            "rust-rcon".to_string(), 
+            "-h".to_string(), "minecraft.lan".to_string(),
+            "-P".to_string(), "password".to_string(),
+            "-p".to_string(), "notanumber".to_string(),
+        ];
+        if let Err(why) = eval_args(&args) {
+            assert_eq!(why.to_string(), "invalid digit found in string");
+        } else {
+            println!("expected integer parsing error");
+            assert!(false);
+        }
+
+        // invalid port (negative)
+        let args = vec![
+            "rust-rcon".to_string(), 
+            "-h".to_string(), "minecraft.lan".to_string(),
+            "-P".to_string(), "password".to_string(),
+            "-p".to_string(), "-1".to_string(),
+        ];
+        if let Err(why) = eval_args(&args) {
+            assert_eq!(why.to_string(), "invalid digit found in string");
+        } else {
+            println!("expected integer parsing error");
+            assert!(false);
+        }
+
+        // invalid port (too big)
+        let args = vec![
+            "rust-rcon".to_string(), 
+            "-h".to_string(), "minecraft.lan".to_string(),
+            "-P".to_string(), "password".to_string(),
+            "-p".to_string(), "1000000".to_string(),
+        ];
+        if let Err(why) = eval_args(&args) {
+            assert_eq!(why.to_string(), "number too large to fit in target type");
+        } else {
+            println!("expected integer parsing error");
+            assert!(false);
+        }
     }
 }
+
+
